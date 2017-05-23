ff54ea115c10f6641b795539a602c78ea0331a8f
Gareth Nelson <gareth@garethnelson.com>
2017-05-23 08:42:33 +0100

    drop the C++ crap

--- make_repl.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-mkdir -p build
-cd build
-cmake ..
-make
-
-./bearlangc ../examples/repl.bear
-g++ -std=c++11 -L. -lreadline -lbearlang ../examples/repl.bear.cpp -I../include -I../sexp-cpp/include -o bearrepl
